72cb8e2f3a37af006690d292b1e0cebb1594f250
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-06-22 13:32:38 +0300

    Blog tablosu düzenlendi

--- BlogMvcApp/Controllers/BlogController.cs
+++ BlogMvcApp/Controllers/BlogController.cs
@@ -23,7 +23,7 @@ namespace BlogMvcApp.Controllers
 
         // GET: Blog/Details/5
         public ActionResult Details(int? id)
-        {
+        {   
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
