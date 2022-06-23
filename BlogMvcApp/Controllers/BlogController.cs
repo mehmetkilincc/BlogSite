fa804cd2af397eebdafb616fdb80777c8ffadbda
Mehmet Kılınç <mehmetkilinc2734@gmail.com>
2022-06-23 15:27:21 +0300

    Blog guncelleme formu hazirlandi.

--- BlogMvcApp/Controllers/BlogController.cs
+++ BlogMvcApp/Controllers/BlogController.cs
@@ -85,13 +85,24 @@ namespace BlogMvcApp.Controllers
         // daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Baslik,Aciklama,Resim,Icerik,EklenmeTarihi,Onay,Anasayfa,CategoryId")] Blog blog)
+        public ActionResult Edit([Bind(Include = "Id,Baslik,Aciklama,Resim,Icerik,Onay,Anasayfa,CategoryId")] Blog blog)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(blog).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var entity = db.Bloglar.Find(blog.Id);
+                if (entity != null)
+                {
+                    entity.Baslik = blog.Baslik;
+                    entity.Aciklama = blog.Aciklama;
+                    entity.Resim = blog.Resim;
+                    entity.Icerik = blog.Icerik;
+                    entity.Onay = blog.Onay;
+                    entity.Anasayfa = blog.Anasayfa;
+                    entity.CategoryId = blog.CategoryId;
+                    db.SaveChanges();
+                    TempData["Blog"] = entity;
+                    return RedirectToAction("Index");
+                }                     
             }
             ViewBag.CategoryId = new SelectList(db.Kategoriler, "Id", "KategoriAdi", blog.CategoryId);
             return View(blog);
